6b72ac68233f6fde30208ba3344d5c10a25c0b4c
Yannick Prudent <yannick.prudent@irt-saintexupery.com>
2022-09-07 14:45:35 +0200

    add PICNN module

---
+++ cnqr/_src/convex.py
@@ -0,0 +1,213 @@
+# This file is part of CNQR which is released under the Apache License 2.0.
+# See file LICENSE in the root directory or https://opensource.org/licenses/Apache-2.0 for full license details.
+
+"""Implementation of Amos+(2017) partially input convex neural networks (PICNN)
+with (slightly modified) initialization schemes proposed in Bunne+(2022).
+
+Largely inspired by ott-jax package: https://github.com/ott-jax/ott distributed under Apache license.
+ """
+from functools import partial
+from typing import Any, Callable, Sequence, Tuple
+
+import jax
+import jax.numpy as jnp
+from flax import linen as nn
+from jax.nn import initializers
+from jax import random
+
+from ott.core.layers import PosDefPotentials, PositiveDense
+
+Shape = Tuple[int]
+Dtype = Any
+Array = Any
+KeyArray = random.KeyArray
+DTypeLikeInexact = Any  # DTypeLikeFloat | DTypeLikeComplex
+
+class DenseWrapper(nn.Module):
+    features: int
+    name: str = 'dense'
+    use_bias: bool = True
+    bias_init_value: float = 0.0
+    kernel_init_type: float = 'lecun_normal'  # alternatives: ['normal', 'identity', 'constant']
+    kernel_epsilon_init: float = 1e-2
+    kernel_constant_init: float = 1.0
+    layer_class: nn.Module = nn.Dense  # alternative: PositiveDense
+
+    @staticmethod
+    def _identity_initializer(key: KeyArray,
+                              shape: Shape,
+                              dtype: DTypeLikeInexact = jnp.float_) -> Array:
+        return jnp.eye(N=shape[-2], M=shape[-1], dtype=dtype).reshape(shape)
+
+    @nn.compact
+    def __call__(self, x, **kwargs):
+        # definine initialization of weights and biases
+        kernel_init_dict = {
+            'lecun_normal': nn.initializers.lecun_normal(),
+            'normal': nn.initializers.normal(self.kernel_epsilon_init),
+            'identity': self._identity_initializer,
+            'constant': initializers.constant(self.kernel_constant_init)
+        }
+        kernel_init = kernel_init_dict[self.kernel_init_type]
+
+        f = self.layer_class(
+            self.features,
+            name=self.name,
+            use_bias=self.use_bias,
+            kernel_init=kernel_init,
+            bias_init=nn.initializers.constant(self.bias_init_value),
+            **kwargs
+        )
+        return f(x)
+
+
+class PICNN(nn.Module):
+    """Partially input convex neural network (PICNN) architecture with initialization.
+    Implementation of partially input convex neural networks as introduced in
+    Amos+(2017) with initialization schemes proposed by Bunne+(2022).
+
+    Args:
+      * dim_hidden: sequence specifying size of hidden dimensions. The
+        output dimension of the last layer is 1 by default.
+      * dim_y: data dimensionality (default: 2).
+      * init_fn: choice of initialization method for weight matrices (default:
+        `jax.nn.initializers.normal`).
+      * epsilon_init: value of standard deviation of weight initialization method.
+      * tau_act_fn: choice of activation function used in context network architecture
+      * sigma_act_fn: choice of activation function used in convex network architecture
+      * pos_act_fn: choice of positive activation function (default: `nn.relu`)
+      * context_dense_layer: choice of layer type for the context modules
+        (default: nn.Dense, later BjorckDense might be used)
+      * gaussian_map: data inputs of source and target measures for
+        initialization scheme based on Gaussian approximation of input and
+        target measure (if None, identity initialization is used).
+    """
+
+    dim_hidden: Sequence[int]
+    dim_y: int = 2
+    init_fn: Callable = jax.nn.initializers.normal
+    epsilon_init: float = 1e-2
+    tau_act_fn: Callable = nn.softplus  # alternative -> GroupSort
+    sigma_act_fn: Callable = nn.softplus  # alternative -> ReLU, ELU
+    pos_act_fn: Callable = nn.relu
+    context_dense_layer: nn.Module = nn.Dense  # TODO: try with BjorckDense
+    gaussian_map: Tuple[jnp.ndarray, jnp.ndarray] = None
+
+    @nn.compact
+    def __call__(self, x, y):
+        """Forward pass.
+
+        Args:
+            x: context variable.
+                array of shape (B, n_features_x) with B the batch_size.
+            y: observation from source distribution.
+                array of shape (B, n_features_y) with B the batch_size.
+
+        Returns:
+            outputs: array of shape (B,)
+        """
+        num_hidden = len(self.dim_hidden)
+
+        # ===== Input encoding =====
+        # encoding the input of context network (upscale to dim_hidden[0])
+        phi = DenseWrapper(
+            features=self.dim_hidden[0],
+            name='phi_dense',
+            kernel_init_type='lecun_normal',
+            use_bias=True,
+            bias_init_value=0.0
+        )
+
+        # encoding the input of convex network (quadratic potential of y)
+        q = PosDefPotentials(
+            self.dim_y,
+            name='q_potential',
+            num_potentials=1,
+            kernel_init=DenseWrapper._identity_initializer,
+            bias_init=nn.initializers.zeros,
+            use_bias=True,
+        )
+
+        # ===== Context network =====
+        # (now simple dense, then will be replaced with lipschitz)
+        # dense layers (with bias), init: weights -> identity, bias -> 0
+        f_x = partial(
+            DenseWrapper,
+            kernel_init_type='identity',
+            use_bias=True,
+            bias_init_value=0
+        )
+        output_dims_x = self.dim_hidden
+
+        # ===== Context modulators =====
+        # f_u: weights init -> approx(0), bias init -> 1.0
+        f_u = partial(
+            DenseWrapper,
+            kernel_init_type='normal',
+            kernel_epsilon_init=self.epsilon_init,
+            use_bias=True,
+            bias_init_value=1.0
+        )
+        output_dims_u = [1] + list(self.dim_hidden)[1:] + [1]
+
+        # f_v: weights init -> approx(0), bias init -> 0.0
+        f_v = partial(
+            DenseWrapper,
+            kernel_init_type='normal',
+            kernel_epsilon_init=self.epsilon_init,
+            use_bias=True,
+            bias_init_value=0.0
+        )
+        output_dims_v = [self.dim_y] * num_hidden + [1]
+
+        # f_w: weights init -> approx(0), bias init -> 0.0
+        f_w = partial(
+            DenseWrapper,
+            kernel_init_type='normal',
+            kernel_epsilon_init=self.epsilon_init,
+            use_bias=True,
+            bias_init_value=0.0
+        )
+        output_dims_w = list(self.dim_hidden) + [1]
+
+        # ===== Convex network =====
+        # w_U:  positive dense layers (no bias), weights init -> ones / d1
+        w_U = partial(
+            DenseWrapper,
+            layer_class=PositiveDense,
+            kernel_init_type='constant',
+            use_bias=False,
+        )
+        rescale = PositiveDense.inv_rectifier_fn
+        kernel_constant_inits_U = [rescale(1.0 / d1) for d1 in [1] + list(self.dim_hidden)]
+        output_dims_U = list(self.dim_hidden) + [1]
+
+        # w_V: dense layers (no bias), weights init -> approx(0)
+        w_V = partial(
+            DenseWrapper,
+            kernel_init_type='normal',
+            kernel_epsilon_init=self.epsilon_init,
+            use_bias=False,
+        )
+        output_dims_V = list(self.dim_hidden) + [1]
+
+        # ===== Inference =====
+        x = phi(x)
+        z = q(y).reshape(x.shape[0], -1)
+        num_steps = len(self.dim_hidden) + 1
+        for i in range(num_steps):
+            # context modulators
+            u = f_u(features=output_dims_u[i], name=f'f_u_dense_{i}')(x)
+            v = f_v(features=output_dims_v[i], name=f'f_v_dense_{i}')(x)
+            w = f_w(features=output_dims_w[i], name=f'f_w_dense_{i}')(x)
+            # convex network
+            w_Uk = w_U(features=output_dims_U[i], name=f'w_U_pdense_{i}',
+                       kernel_constant_init=kernel_constant_inits_U[i])
+            w_Vk = w_V(features=output_dims_V[i], name=f'w_V_dense_{i}')
+            h = w_Uk(z * self.pos_act_fn(u)) + w_Vk(y * v) + w
+            z = self.sigma_act_fn(h)
+            # context network
+            if i < num_steps - 1:
+                x = f_x(features=output_dims_x[i], name=f'f_x_dense_{i}')(x)
+                x = self.tau_act_fn(x)
+        return z.reshape(-1,)
---
+++ tests/convex_test.py
@@ -0,0 +1,105 @@
+# This file is part of CNQR which is released under the Apache License 2.0.
+# See file LICENSE in the root directory or https://opensource.org/licenses/Apache-2.0 for full license details.
+
+"""Tests for PICNN network architecture."""
+from absl.testing import parameterized
+from absl.testing import absltest
+
+import jax
+import jax.numpy as jnp
+import jax._src.test_util as jtu
+import flax.linen as nn
+
+from cnqr._src.convex import PICNN
+
+
+class PICNNTest(jtu.JaxTestCase):
+
+    @parameterized.parameters((10, 2, 4, (64, 64), 1), (10, 5, 5, (64, 64), 0.01), (10, 4, 2, (64, 64, 64), 0.1))
+    def test_picnn_convexity(self, n_samples, n_features_x, n_features_y, dim_hidden, epsilon_init):
+        """Tests convexity of PICNN (wrt y) with identity initialization and with random
+          initialization (higher values of epislon)."""
+        rng = jax.random.PRNGKey(999)
+        # define picnn model
+        picnn = PICNN(dim_hidden, dim_y=n_features_y, epsilon_init=epsilon_init)
+
+        # initialize model
+        key1, key2, key3 = jax.random.split(rng, 3)
+        params = picnn.init(key1, x=jnp.ones((1, n_features_x)), y=jnp.ones((1, n_features_y)))['params']
+
+        # check convexity
+        x = jax.random.normal(key1, (n_samples, n_features_x))
+        y1 = jax.random.normal(key2, (n_samples, n_features_y))
+        y2 = jax.random.normal(key3, (n_samples, n_features_y))
+
+        out_y1 = picnn.apply({'params': params}, x=x, y=y1)
+        out_y2 = picnn.apply({'params': params}, x=x, y=y2)
+
+        out = list()
+        for t in jnp.linspace(0, 1, num=25):
+            out_interp = picnn.apply({'params': params}, x=x, y=t * y1 + (1 - t) * y2)
+            out.append((t * out_y1 + (1 - t) * out_y2) - out_interp)
+        tol = 1e-5
+        self.assertTrue(jnp.all(jnp.stack(out) + tol >= 0))
+
+    @parameterized.parameters((10, 2, 4, (64, 64), 1), (10, 5, 5, (64, 64), 0.01), (10, 4, 2, (64, 64, 64), 0.1))
+    def test_picnn_hessian(self, n_samples, n_features_x, n_features_y, dim_hidden, epsilon_init):
+        """Tests if Hessian of PICNN (wrt y) is positive-semidefinite with identity initialization and with
+          random initialization (higher values of epislon)."""
+        rng = jax.random.PRNGKey(999)
+        # define picnn model
+        picnn = PICNN(dim_hidden, dim_y=n_features_y, epsilon_init=epsilon_init)
+
+        # initialize model
+        key1, key2, key3 = jax.random.split(rng, 3)
+        params = picnn.init(key1, x=jnp.ones((1, n_features_x)), y=jnp.ones((1, n_features_y)))['params']
+
+        # check if Hessian is positive-semidefinite via eigenvalues
+        x_data = jax.random.normal(key2, (n_samples, n_features_x))
+        y_data = jax.random.normal(key3, (n_samples, n_features_y))
+
+        # compute Hessian
+        W = []
+        for x_i, y_i in zip(x_data, y_data):
+            hessian = jax.jacfwd(jax.jacrev(picnn.apply, argnums=2), argnums=2)  # wrt y
+            picnn_hess = hessian({'params': params}, x_i[jnp.newaxis, ...], y_i[jnp.newaxis, ...])\
+                .reshape((n_features_y, n_features_y))
+            # compute eigenvalues
+            w, _ = jnp.linalg.eig(picnn_hess)
+            W.append(w)
+        tol = 1e-5
+        self.assertTrue(jnp.all(jnp.stack(W) + tol >= 0))
+
+    @parameterized.parameters((10, 2, 4, (64, 64)), (10, 5, 5, (64, 64)), (10, 4, 2, (64, 64, 64)))
+    def test_picnn_grad_identity_at_initialization(self, n_samples, n_features_x, n_features_y, dim_hidden):
+        """Tests if the gradient of a PICNN (wrt y) is equivalent to the identity function at initialization"""
+        rng = jax.random.PRNGKey(999)
+        # define picnn model
+        picnn = PICNN(dim_hidden, dim_y=n_features_y, epsilon_init=1e-5,
+                      tau_act_fn=nn.softplus, sigma_act_fn=nn.relu, pos_act_fn=nn.relu)
+
+        # initialize model
+        key1, key2, key3 = jax.random.split(rng, 3)
+        params = picnn.init(key1, x=jnp.ones((1, n_features_x)), y=jnp.ones((1, n_features_y)))['params']
+
+        # check if transport function is initially close to identity function
+        x_data = jax.random.normal(key2, (n_samples, n_features_x))
+        y_data = jax.random.normal(key3, (n_samples, n_features_y))
+
+        # compute grads
+        batch_forward = lambda x, y: picnn.apply({'params': params}, x[jnp.newaxis, ...], y[jnp.newaxis, ...])[0]
+        grads = jax.vmap(jax.grad(batch_forward, argnums=1), in_axes=(0, 0))(x_data, y_data)
+        rtol, atol = 1e-3, 1e-4
+        self.assertAllClose(y_data, grads, atol=atol, rtol=rtol)
+
+
+def picnn_summary(n_features_x, n_features_y, dim_hidden):
+    """Summary of PICNN module"""
+    picnn = PICNN(dim_hidden, dim_y=n_features_y)
+    print(picnn.tabulate(jax.random.PRNGKey(0), jnp.ones((1, n_features_x)), jnp.ones((1, n_features_y))))
+
+
+if __name__ == '__main__':
+    picnn_summary(2, 2, (64, 64))
+    jax.config.update("jax_enable_x64", False)
+    absltest.main()
